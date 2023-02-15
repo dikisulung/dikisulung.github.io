87b52d2a11eb0e31dce631468fde728a9383d89e
dikisulung <dikisulung01@gmail.com>
2023-02-15 15:01:19 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $('#menu').click(function () {
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load', function () {
+    $(this).removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+  });
+});
+
+var textWrapper = document.querySelector('.hello');
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: '.hello .letter',
+    translateX: [40, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: 'easeOutExpo',
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i,
+  })
+  .add({
+    targets: '.hello .letter',
+    translateX: [0, -30],
+    opacity: [1, 0],
+    easing: 'easeInExpo',
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i,
+  });
